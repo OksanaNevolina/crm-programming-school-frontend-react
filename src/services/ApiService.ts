2adfe2b748d2f0f2423a68c1163bcb96bcb4f941
OksanaNevolina <onevolina5gmail.com>
2024-09-25 21:56:16 +0300

    added interceptors.response

--- src/services/ApiService.ts
+++ src/services/ApiService.ts
@@ -1,6 +1,7 @@
-import axios from 'axios';
+import axios, { AxiosError } from 'axios';
 
-import { baseURL } from '../constants';
+import { baseURL, urls } from '../constants';
+import { router } from '../router';
 import { AuthService } from './AuthService';
 
 const ApiService = axios.create({ baseURL });
@@ -12,4 +13,53 @@ ApiService.interceptors.request.use((request) => {
   }
   return request;
 });
+
+let isRefreshing = false;
+
+const waitList: IWaitList[] = [];
+ApiService.interceptors.response.use(
+  (responce) => {
+    return responce;
+  },
+  async (error: AxiosError) => {
+    const originalRequest = error.config;
+
+    if (error.response.status === 401) {
+      if (!isRefreshing) {
+        isRefreshing = true;
+        try {
+          await AuthService.refresh();
+          isRefreshing = false;
+          afterRefresh();
+          return ApiService(originalRequest);
+        } catch (e) {
+          AuthService.deleteTokens();
+          isRefreshing = false;
+          router.navigate('/login?SessionExpired=true');
+          return Promise.reject(error);
+        }
+      }
+      if (originalRequest.url === urls.auth.refresh) {
+        return Promise.reject(error);
+      }
+
+      return new Promise((resolve) => {
+        subscribeToWaitList(() => resolve(ApiService(originalRequest)));
+      });
+    }
+
+    return Promise.reject(error);
+  },
+);
+type IWaitList = () => void;
+const subscribeToWaitList = (cb: IWaitList): void => {
+  waitList.push(cb);
+};
+
+const afterRefresh = (): void => {
+  while (waitList.length) {
+    const cb = waitList.pop();
+    cb();
+  }
+};
 export { ApiService };
